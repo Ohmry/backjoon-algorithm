65ea9857738177f336c941d51f3509ee93249ae5
Ohmry <ohmry@Ohmryui-MacBookPro.local>
2022-01-09 17:47:43 +0900

    upload P2292

---
+++ P2292/Main.java
@@ -0,0 +1,22 @@
+package backjoon.P2292;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int goal = scanner.nextInt();
+        int maxRoom = 1;
+        int count = 1;
+
+        while (goal > maxRoom) {
+            maxRoom += 6 * count;
+            count++;
+        }
+
+        System.out.println(count);
+
+        scanner.close();
+        return;
+    }
+}
